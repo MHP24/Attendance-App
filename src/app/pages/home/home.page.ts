31699aa5f97c1ac8d3f99fbdab3add2e78042b29
MHP24 <85582472+MHP24@users.noreply.github.com>
2022-09-06 22:41:53 -0300

    Animations delete (@angular/animations)

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { AnimationController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
-  animations: [
-    trigger('enterView', [
-      state('void', style({
-        transform: 'translateX(100%)',
-        opacity: 0
-      })),
-      transition(':enter', [
-        animate(400, style({
-          transform: 'translateX(0)',
-          opacity: 1
-        }))
-      ])
-    ])
-  ]
+  styleUrls: ['./home.page.scss']
 })
 
 export class HomePage implements OnInit {
+  @ViewChild('pageTitle', { read: ElementRef, static: true}) pageTitle: ElementRef;
+
   username: string;
   constructor(private readonly router: Router,
-    private readonly activatedRoute: ActivatedRoute) { }
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly animationController: AnimationController) { }
   ngOnInit() {
-    this.activatedRoute.queryParams.subscribe( params => {
+    this.activatedRoute.queryParams.subscribe( () => {
       const { name } = JSON.parse(this.router.getCurrentNavigation().extras.state.user);
       this.username = name;
     });
 
     /* Local storage method */
     // this.username = JSON.parse(localStorage.getItem('user')).name;
+  }
+
+  ngAfterViewInit() {
+    const animation = this.animationController
+    .create()
+    .addElement(this.pageTitle.nativeElement)
+    .iterations(1)
+    .duration(400)
+    .fromTo('transform', 'translateX(100%)', 'translate(0%)')
+    animation.play();
   }
 
   onClick(): void {
